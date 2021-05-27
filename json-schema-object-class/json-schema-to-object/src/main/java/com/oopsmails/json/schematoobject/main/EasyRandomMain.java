2c7399d3a66e7a6fad2fe5f2b76d7dfb5d6fd6f4
oopsmails <oopsmails@gmail.com>
2021-05-27 09:26:35 -0400

    20210527: EasyRandomMain

--- json-schema-object-class/json-schema-to-object/src/main/java/com/oopsmails/json/schematoobject/main/EasyRandomMain.java
+++ json-schema-object-class/json-schema-to-object/src/main/java/com/oopsmails/json/schematoobject/main/EasyRandomMain.java
@@ -5,11 +5,73 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.oopsmails.json.schematoobject.model.Employee;
 import org.jeasy.random.EasyRandom;
+import org.jeasy.random.EasyRandomParameters;
+import org.jeasy.random.TypePredicates;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Random;
+
+import static java.nio.charset.Charset.forName;
+
+/**
+ * https://github.com/j-easy/easy-random
+ *
+ * <dependency>
+ *     <groupId>org.jeasy</groupId>
+ *     <artifactId>easy-random-core</artifactId>
+ *     <version>4.0.0</version>
+ * </dependency>
+ *
+ * EasyRandomParameters parameters = new EasyRandomParameters()
+ *    .seed(123L)
+ *    .objectPoolSize(100)
+ *    .randomizationDepth(3)
+ *    .charset(forName("UTF-8"))
+ *    .timeRange(nine, five)
+ *    .dateRange(today, tomorrow)
+ *    .stringLengthRange(5, 50)
+ *    .collectionSizeRange(1, 10)
+ *    .scanClasspathForConcreteTypes(true)
+ *    .overrideDefaultInitialization(false)
+ *    .ignoreRandomizationErrors(true);
+ *
+ * EasyRandomParameters parameters = new EasyRandomParameters()
+ *    .randomize(String.class, () -> "foo")
+ *    .excludeField(named("age").and(ofType(Integer.class)).and(inClass(Person.class)));
+ *
+ * parameters.excludeField(FieldPredicates.named("lastName").and(FieldPredicates.inClass(Employee.class)));
+ * parameters.excludeType(TypePredicates.inPackage("not.existing.pkg"));
+ * parameters.randomize(YearQuarter.class, new YearQuarterRandomizer());
+ *
+ *
+ */
+
 
 public class EasyRandomMain {
     public static void main(String[] args) {
         try {
-            EasyRandom easyRandom = new EasyRandom();
+            EasyRandomParameters parameters = new EasyRandomParameters()
+                    .seed(123L)
+                    .objectPoolSize(100)
+                    .randomizationDepth(3)
+                    .charset(forName("UTF-8"))
+                    .stringLengthRange(5, 50)
+                    .collectionSizeRange(1, 10)
+                    .scanClasspathForConcreteTypes(true)
+                    .overrideDefaultInitialization(false)
+                    .ignoreRandomizationErrors(true);
+
+            Random random = new Random();
+            parameters.randomize(BigDecimal.class, () -> new BigDecimal(BigInteger.valueOf(random.nextInt(100001)), 2));
+            parameters.randomize(int.class, () -> random.nextInt(1200 - 55) + 55);
+            parameters.randomize(Integer.class, () -> random.nextInt(1200 - 55) + 55);
+            parameters.randomize(long.class, () -> 10L + (long) (Math.random() * (1888L - 10L)));
+            parameters.randomize(Long.class, () -> 10L + (long) (Math.random() * (1888L - 10L)));
+
+            parameters.excludeType(TypePredicates.ofType(EasyRandomMain.class)); // e.g, exclude for no default constructor
+
+            EasyRandom easyRandom = new EasyRandom(parameters);
             Employee employee = easyRandom.nextObject(Employee.class);
             System.out.println(employee);
 
