59e6796bf4529793403fe82c2b6e4962bd7dba84
oopsmails <oopsmails@gmail.com>
2021-05-26 23:57:20 -0400

    20210526: json-schema-object-class

--- json-schema-object-class/json-schema-to-object/src/main/java/com/oopsmails/json/schematoobject/main/EasyRandomMain.java
+++ json-schema-object-class/json-schema-to-object/src/main/java/com/oopsmails/json/schematoobject/main/EasyRandomMain.java
@@ -1,12 +1,25 @@
 package com.oopsmails.json.schematoobject.main;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import com.oopsmails.json.schematoobject.model.Employee;
 import org.jeasy.random.EasyRandom;
 
 public class EasyRandomMain {
     public static void main(String[] args) {
-        EasyRandom easyRandom = new EasyRandom();
-        Employee employee = easyRandom.nextObject(Employee.class);
-        System.out.println(employee);
+        try {
+            EasyRandom easyRandom = new EasyRandom();
+            Employee employee = easyRandom.nextObject(Employee.class);
+            System.out.println(employee);
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            ObjectWriter prettyPrinter = objectMapper.writerWithDefaultPrettyPrinter();
+
+            String json = prettyPrinter.writeValueAsString(employee);
+            System.out.println(json);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
     }
 }
