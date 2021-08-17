a207dd66a81afc91233f8daa751731c8a45c27f4
oopsmails <oopsmails@gmail.com>
2021-08-17 11:18:53 -0400

    20210817: wiremock junit usage updated

--- wiremock/wiremock-junit/src/test/java/com/oopsmails/wiremock/scenario/WireMockScenarioExampleIntegrationTest.java
+++ wiremock/wiremock-junit/src/test/java/com/oopsmails/wiremock/scenario/WireMockScenarioExampleIntegrationTest.java
@@ -1,25 +1,23 @@
 package com.oopsmails.wiremock.scenario;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static com.github.tomakehurst.wiremock.client.WireMock.get;
-import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
-import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
-import static org.junit.Assert.assertEquals;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import com.github.tomakehurst.wiremock.stubbing.Scenario;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.junit.Rule;
 import org.junit.Test;
 
-import com.github.tomakehurst.wiremock.junit.WireMockRule;
-import com.github.tomakehurst.wiremock.stubbing.Scenario;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.junit.Assert.assertEquals;
 
 public class WireMockScenarioExampleIntegrationTest {
     private static final String THIRD_STATE = "third";
@@ -52,6 +50,36 @@ public class WireMockScenarioExampleIntegrationTest {
         assertEquals(TIP_01, nextTip());
     }
 
+    @Test
+    public void assertWiremockSetup() throws IOException {
+        // Arrange - setup wiremock stubs
+        configureStubs();
+
+        // execute request through the http client
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("http://localhost:8080/test/abc")
+                .get()
+                .build();
+
+        // Act - call the endpoint
+        Response response = client.newCall(request).execute();
+
+        // Assert - verify the response
+        assertEquals("Test success!", response.body().string());
+        verify(exactly(1), getRequestedFor(urlEqualTo("/test/abc")));
+
+    }
+
+    // configure stubs for wiremock
+    private void configureStubs() {
+        configureFor("localhost", 8080);
+        stubFor(get(urlEqualTo("/test/abc"))
+                .willReturn(aResponse().withBody("Test success!")));
+    }
+
+
     private void createWireMockStub(String currentState, String nextState, String responseBody) {
         stubFor(get(urlEqualTo("/java-tip"))
                 .inScenario("java tips")
@@ -64,7 +92,7 @@ public class WireMockScenarioExampleIntegrationTest {
         );
     }
 
-    private String nextTip() throws ClientProtocolException, IOException {
+    private String nextTip() throws IOException {
         CloseableHttpClient httpClient = HttpClients.createDefault();
         HttpGet request = new HttpGet(String.format("http://localhost:%s/java-tip", port));
         HttpResponse httpResponse = httpClient.execute(request);
